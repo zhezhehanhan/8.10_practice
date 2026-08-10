97d182154f693268a28f8188c8a156640051446d
zhezhehanhan <1747830596@qq.com>
2026-08-10 09:50:07 +0800

    题目1

--- common/src/command_pack_queue.c
+++ common/src/command_pack_queue.c
@@ -16,35 +16,60 @@
 /* TODO: 初始化队列，将 head/tail/count 归零 */
 void packet_queue_init(packet_queue *q)
 {
-    
+    q->head=0;
+    q->tail=0;
+    q->count=0;
     /* 在此实现 */
 }
 
 /* TODO: 入队，成功返回 true，队列满返回 false */
 bool packet_queue_push(packet_queue *q, const command_packet *pkt)
 {
-    
+    if(packet_queue_is_full(&q))
+    {
+        q->count++;
+        return false;
+    }
+    q->buf[q->tail]= *pkt;
+    q->tail=(q->tail+1U)%PACKET_QUEUE_SIZE;
+    q->count++;
+    return true;
     /* 在此实现 */
 }
 
 /* TODO: 出队，将数据写入 *pkt，成功返回 true，队列空返回 false */
 bool packet_queue_pop(packet_queue *q, command_packet *pkt)
 {
-    
+    if( packet_queue_is_empty(&q))
+    {
+        return false;
+    }
+    *pkt=q->buf[q->head];
+    q->head=(q->head+1U)%PACKET_QUEUE_SIZE;
+    q->count++;
+    return true;
     /* 在此实现 */
 }
 
 /* TODO: 判空 */
 bool packet_queue_is_empty(const packet_queue *q)
 {
-    
+    if(q->head==q->tail)
+    {
+        return true;
+    }
+    return false;
     /* 在此实现 */
 }
 
 /* TODO: 判满 */
 bool packet_queue_is_full(const packet_queue *q)
 {
-   
+    if((q->head+1U)%PACKET_QUEUE_SIZE==q->tail)
+    {
+        return true;
+    }
+    return false;
     /* 在此实现 */
 }
 
